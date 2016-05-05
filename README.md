cf721c349bea4090470eae714afb9fc4da82aaf4
Mehmet Kılıç <mail@mehmetkilic.com.tr>
2016-05-05 16:13:13 +0300

    larCouch first commit

--- README.md
+++
@@ -1,3 +0,0 @@
-Laravel 4.* CouchDB Library
-
-Laravel 4.* is an Eloquent model and Query builder with support for CouchDB, using the original Laravel API. This library extends the original Laravel classes, so it uses exactly the same methods.
---
+++ mehmetkilic/larCouch/src/CouchDB.php
@@ -0,0 +1,16 @@
+<?php
+
+  /**
+   * @todo    Bu sınıf laravel frameworku üzerinden CouchDB ile çalışmaya olanak sunar
+   * @author  Mehmet Kılıç | mail@mehmetkilic.com.tr
+  */
+  namespace mehmetkilic\laravel_couchdb;
+
+  class CouchDB
+  {
+    public function List()
+    {
+      $array = "Class autoloader is success";
+      return $array;
+    }
+  }
