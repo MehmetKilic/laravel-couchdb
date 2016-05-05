13c347d27feef03044b8b1ad67f483d071a190c4
Mehmet Kılıç <mail@mehmetkilic.com.tr>
2016-05-05 16:30:47 +0300

    All folder path updating

--- LaraCouchDB/src/CouchDB.php
+++ LaraCouchDB/src/CouchDB.php
@@ -4,7 +4,7 @@
    * @todo    Bu sınıf laravel frameworku üzerinden CouchDB ile çalışmaya olanak sunar
    * @author  Mehmet Kılıç | mail@mehmetkilic.com.tr
   */
-  namespace mehmetkilic\laravel_couchdb;
+  namespace mehmetkilic\LaraCouchDB;
 
   class CouchDB
   {
